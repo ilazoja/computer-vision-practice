9143c67829488d01797c747904adf464249d5cc1
HOLOHEART\Ilir <ilir.lazoja@sri.utoronto.ca>
2017-11-27 17:59:42 -0500

    small changes

--- ProblemSets/ps4/ps4.m
+++ ProblemSets/ps4/ps4.m
@@ -13,21 +13,20 @@ transB = imread(fullfile('input', 'transB.jpg'));
 %% 1-a
 [Ix1A, Iy1A, I1A] = computeGradients(simA);
 [Ix1B, Iy1B, I1B] = computeGradients(simB);
-% imshow(I1A);
-% [Ix2A, Iy2A, I1A] = computeGradients(transA);
-% [Ix2B, Iy2B, I2B] = computeGradients(transB);
+[Ix2A, Iy2A, I1A] = computeGradients(transA);
+[Ix2B, Iy2B, I2B] = computeGradients(transB);
 
 %% 1-b
 Rm1A = computeHarrisValues(Ix1A, Iy1A, 0.04, filter_size, filter_sigma);
 Rm1B = computeHarrisValues(Ix1B, Iy1B, 0.04, filter_size, filter_sigma);
-% Rm2A = computeHarrisValues(Ix2A, Iy2A, 0.04, filter_size, filter_sigma);
-% Rm2B = computeHarrisValues(Ix2B, Iy2B, 0.04, filter_size, filter_sigma);
+Rm2A = computeHarrisValues(Ix2A, Iy2A, 0.04, filter_size, filter_sigma);
+Rm2B = computeHarrisValues(Ix2B, Iy2B, 0.04, filter_size, filter_sigma);
 
 %% 1-c
 Rm1A = adjustHarris(Rm1A, 0.5, 5);
 Rm1B = adjustHarris(Rm1B, 0.5, 5);
-% Rm2A = adjustHarris(Rm2A, 0.5, 5);
-% Rm2B = adjustHarris(Rm2B, 0.5, 5);
+Rm2A = adjustHarris(Rm2A, 0.5, 5);
+Rm2B = adjustHarris(Rm2B, 0.5, 5);
 
 %% 2-a
 [selectedAngles1A, points1A] = computeAngles(Rm1A, Ix1A, Iy1A);
@@ -37,9 +36,16 @@ h1 = vl_plotframe(frame);
 [selectedAngles1B, points1B] = computeAngles(Rm1B, Ix1B, Iy1B);
 frame = [points1B(:,1)+size(simA,2) points1B(:,2)  ones(size(points1B,1),1)*10  selectedAngles1B]';
 h2 = vl_plotframe(frame);
+[selectedAngles2A, points2A] = computeAngles(Rm2A, Ix2A, Iy2A);
+[selectedAngles2B, points2B] = computeAngles(Rm2B, Ix2B, Iy2B);
+
 %% 2-b
 Gxy = fspecial('gaussian', filter_size, filter_sigma);
 frame1A = [points1A  ones(size(points1A,1),1)  selectedAngles1A]';
 frame1B = [points1B  ones(size(points1B,1),1)  selectedAngles1B]';
 matchSIFT(simA, simB, frame1A, frame1B, Gxy);
+frame2A = [points2A  ones(size(points2A,1),1)  selectedAngles2A]';
+frame2B = [points2B  ones(size(points2B,1),1)  selectedAngles2B]';
+matchSIFT(transA, transB, frame2A, frame2B, Gxy);
 
+%% 3-a
