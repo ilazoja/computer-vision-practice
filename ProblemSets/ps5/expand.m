90cce5c5a025f72e493501373f236b22c46c7c0c
HOLOHEART\Ilir <ilir.lazoja@sri.utoronto.ca>
2017-11-28 19:16:04 -0500

    tried kron instead

--- ProblemSets/ps5/expand.m
+++ ProblemSets/ps5/expand.m
@@ -1,10 +1,9 @@
-function reducedImg = reduce(img)
+function reducedImg = expand(img)
 %REDUCE Summary of this function goes here
 %   Detailed explanation goes here
-    filter = [1 4 6 4 1]./ 16;
-    reducedImg = conv2(img, filter, 'same');
-    reducedImg = conv2(reducedImg, filter', 'same');
-    reducedImg = reducedImg(1:2:end, 1:2:end);
-    imshow(reducedImg, [0 255]);
+    kw = 5; % default kernel width
+    cw = .375; % kernel centre weight, same as MATLAB func impyramid. 0.6 in the Paper
+    ker1d = [.25-cw/2 .25 cw .25 .25-cw/2];
+    kernel = kron(ker1d,ker1d')*4;;
 end
 
--- ProblemSets/ps5/reduce.m
+++ ProblemSets/ps5/reduce.m
@@ -6,5 +6,11 @@ function reducedImg = reduce(img)
     reducedImg = conv2(reducedImg, filter', 'same');
     reducedImg = reducedImg(1:2:end, 1:2:end);
     imshow(reducedImg, [0 255]);
+    
+    kernelWidth = 5; % default
+cw = .375; % kernel centre weight, same as MATLAB func impyramid. 0.6 in the Paper
+ker1d = [.25-cw/2 .25 cw .25 .25-cw/2];
+kernel = kron(ker1d,ker1d');
+kernel = kron(filter, filter');
 end
 
